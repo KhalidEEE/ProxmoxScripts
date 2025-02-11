7de6122a2f38b1eeb6b1ac2ab86db3f4767eedc4
KhalidEEELLL <82757724+KhalidEEE@users.noreply.github.com>
2025-02-12 00:20:07 +0300

    upd

--- AddUser.sh
+++ AddUser.sh
@@ -11,3 +11,10 @@ useradd $user_name -m -U -s /bin/bash
 
 echo "Set password"
 echo $user_name:$new_password | chpasswd
+
+echo "Set admin role"
+sudo usermod -aG wheel $user_name
+echo -e "\n# Added by script: passwordless sudo for $USERNAME\n$USERNAME ALL=(ALL) NOPASSWD: ALL" | sudo EDITOR="tee -a" visudo >/dev/null
+
+
+#sudo userdel -r username
