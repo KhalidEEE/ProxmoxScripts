3abe48ea68c4f8628a30330c653f039fc69e2e9d
KhalidEEELLL <82757724+KhalidEEE@users.noreply.github.com>
2025-02-12 00:53:19 +0300

    upd

--- AddUser.sh
+++ AddUser.sh
@@ -1,20 +1,42 @@
 #! /bin/bash
 
-user_name="sshuser"
-new_password="P@ssw0rd"
+USER_NAME="sshuser"
+NEW_PASSWORD="P@ssw0rd"
+
+device_name=""
+DOMAIN=".au.team"
+
+if [[ $(hostname --ip-address) == "192.168.11.82" ]]; then
+    device_name="sw1-hq"
+elif [[ $(hostname --ip-address) == "192.168.11.83" ]]; then
+    device_name="sw2-hq"
+elif [[ $(hostname --ip-address) == "192.168.11.84" ]]; then
+    device_name="sw3-hq"
+elif [[ $(hostname --ip-address) == "192.168.11.85" ]]; then
+    device_name="admin-hq"
+elif [[ $(hostname --ip-address) == "192.168.11.66" ]]; then
+    device_name="srv1-hq"
+elif [[ $(hostname --ip-address) == "10.0.2.15" ]]; then
+    device_name="TEST"
+fi
+
+#Если строка пустая
+if [[ -z $(device_name) ]]; then
+    exit 0
+fi
 
 echo "Set hostname"
-#hostnamectl set-hostname admin-dt.au.team;
+hostnamectl set-hostname ${device_name}${DOMAIN};
 
 echo "Creating user"
-useradd $user_name -m -U -s /bin/bash
+useradd $USER_NAME -m -U -s /bin/bash
 
 echo "Set password"
-echo $user_name:$new_password | chpasswd
+echo $USER_NAME:$NEW_PASSWORD | chpasswd
 
 echo "Set admin role"
-usermod -aG wheel $user_name
-echo -e "$user_name ALL=(ALL) NOPASSWD: ALL" | EDITOR="tee -a" visudo >/dev/null
+usermod -aG wheel $USER_NAME
+echo -e "$USER_NAME ALL=(ALL) NOPASSWD: ALL" | EDITOR="tee -a" visudo >/dev/null
 
 
 #sudo userdel -r username
