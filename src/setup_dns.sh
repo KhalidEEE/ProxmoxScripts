54cb6ec5edc2f43b0218655d1b5abec2a18eecbc
KhalidEEE <82757724+KhalidEEE@users.noreply.github.com>
2025-02-21 15:20:49 +0300

    upd

--- src/setup_dns.sh
+++ src/setup_dns.sh
@@ -24,30 +24,28 @@ function setup_bind_srv1_hq() {
 #    sed -i "30s#.*#        allow-query { 192.168.11.0/24; 192.168.33.0/24; };#" $options_path
 #    sed -i "31s#.*#        allow-transfer { 192.168.33.66; };#" $options_path
 
-    python3 -c '
-    options_path = "/etc/bind/options.conf"
-
-    with fileinput.FileInput(options_path, inplace=True) as file:
-        for line in file:
-            if file.lineno() == 16:
-                new_line = "\tlisten-on { 127.0.0.1; 192.168.11.66; };\n"
-                print(new_line, end="")
-            elif file.lineno() == 17:
-                new_line = "\tlisten-on-v6 { any; };\n"
-                print(new_line, end="")
-            elif file.lineno() == 24:
-                new_line = "\tforwarders { 77.88.8.8; };\n"
-                print(new_line, end="")
-            elif file.lineno() == 29:
-                new_line = "\tallow-query { 192.168.11.0/24; 192.168.33.0/24; };\n"
-                print(new_line, end="")
-            elif file.lineno() == 30:
-                new_line = "\t8allow-transfer { 192.168.33.66; };\n\n"
-                print(new_line, end="")
-            else:
-                print(line, end="")
-    '
-
+python3 - ./db.csv << EOF
+options_path = "/etc/bind/options.conf"
+with fileinput.FileInput(options_path, inplace=True) as file:
+    for line in file:
+        if file.lineno() == 16:
+            new_line = "\tlisten-on { 127.0.0.1; 192.168.11.66; };\n"
+            print(new_line, end="")
+        elif file.lineno() == 17:
+            new_line = "\tlisten-on-v6 { any; };\n"
+            print(new_line, end="")
+        elif file.lineno() == 24:
+            new_line = "\tforwarders { 77.88.8.8; };\n"
+            print(new_line, end="")
+        elif file.lineno() == 29:
+            new_line = "\tallow-query { 192.168.11.0/24; 192.168.33.0/24; };\n"
+            print(new_line, end="")
+        elif file.lineno() == 30:
+            new_line = "\t8allow-transfer { 192.168.33.66; };\n\n"
+            print(new_line, end="")
+        else:
+            print(line, end="")
+EOF
 
     systemctl enable --now bind
     printf "search au.team\nnameserver 192.168.11.66" > /etc/net/ifaces/enp7s1/resolv.conf
