2e6f41de174de72c4d0035bb8bfd0ae10f45e55c
KhalidEEE <82757724+KhalidEEE@users.noreply.github.com>
2025-02-21 15:37:39 +0300

    upd

--- src/setup_dns.sh
+++ src/setup_dns.sh
@@ -18,35 +18,29 @@ function setup_bind_srv1_hq() {
 
     cp -r $options_path $options_path.bak
 
-#    sed -i "16s#.*#        listen-on { 127.0.0.1; 192.168.11.66; };#" $options_path
-#    sed -i "17s#.*#        listen-on-v6 { any; };#" $options_path
-#    sed -i "24s#.*#        forwarders { 77.88.8.8; };#" $options_path
-#    sed -i "30s#.*#        allow-query { 192.168.11.0/24; 192.168.33.0/24; };#" $options_path
-#    sed -i "31s#.*#        allow-transfer { 192.168.33.66; };#" $options_path
+    python3 - ./db.csv << EOF
+    import fileinput
 
-python3 - ./db.csv << EOF
-import fileinput
-
-options_path = "/etc/bind/options.conf"
-with fileinput.FileInput(options_path, inplace=True) as file:
-    for line in file:
-        if file.lineno() == 16:
-            new_line = "\tlisten-on { 127.0.0.1; 192.168.11.66; };\n"
-            print(new_line, end="")
-        elif file.lineno() == 17:
-            new_line = "\tlisten-on-v6 { any; };\n"
-            print(new_line, end="")
-        elif file.lineno() == 24:
-            new_line = "\tforwarders { 77.88.8.8; };\n"
-            print(new_line, end="")
-        elif file.lineno() == 29:
-            new_line = "\tallow-query { 192.168.11.0/24; 192.168.33.0/24; };\n"
-            print(new_line, end="")
-        elif file.lineno() == 30:
-            new_line = "\t8allow-transfer { 192.168.33.66; };\n\n"
-            print(new_line, end="")
-        else:
-            print(line, end="")
+    options_path = "/etc/bind/options.conf"
+    with fileinput.FileInput(options_path, inplace=True) as file:
+        for line in file:
+            if file.lineno() == 16:
+                new_line = "\tlisten-on { 127.0.0.1; 192.168.11.66; };\n"
+                print(new_line, end="")
+            elif file.lineno() == 17:
+                new_line = "\tlisten-on-v6 { any; };\n"
+                print(new_line, end="")
+            elif file.lineno() == 24:
+                new_line = "\tforwarders { 77.88.8.8; };\n"
+                print(new_line, end="")
+            elif file.lineno() == 29:
+                new_line = "\tallow-query { 192.168.11.0/24; 192.168.33.0/24; };\n"
+                print(new_line, end="")
+            elif file.lineno() == 30:
+                new_line = "\t8allow-transfer { 192.168.33.66; };\n\n"
+                print(new_line, end="")
+            else:
+                print(line, end="")
 EOF
 
     systemctl enable --now bind
