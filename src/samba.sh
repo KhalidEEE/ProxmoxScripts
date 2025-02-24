cff95102e2c1d04f38754767d6bc3bbc3bc0c372
KhalidEEELLL <82757724+KhalidEEE@users.noreply.github.com>
2025-02-24 18:12:10 +0300

    upd

--- src/samba.sh
+++ src/samba.sh
@@ -7,7 +7,7 @@ BIND_PATH="/etc/bind/options.conf"
 KDC_PATH="/etc/krb5.conf"
 SMB_PATH="SMB_PATH=""/etc/samba/smb.conf"
 
-apt-get install task-samba-dc -y
+admin_passwd="P@ssw0rd"
 
 configuring_srv-hq () {
 
@@ -38,7 +38,7 @@ configuring_srv-hq () {
       --domain=AU \
       --server-role=dc \
       --dns-backend=BIND9_DLZ \
-      --adminpass='P@ssw0rd'
+      --adminpass="${admin_passwd}"
 
 
     systemctl enable --now samba
@@ -60,53 +60,53 @@ configuring_srv-hq () {
 }
 
 adding_all_entries_srv-hq () {
-    samba-tool dns add 127.0.0.1 au.team r-dt A 192.168.33.89
-    samba-tool dns add 127.0.0.1 au.team fw-dt A 192.168.33.90
-    samba-tool dns add 127.0.0.1 au.team fw-dt A 192.168.33.1
-    samba-tool dns add 127.0.0.1 au.team fw-dt A 192.168.33.65
-    samba-tool dns add 127.0.0.1 au.team fw-dt A 192.168.33.81
-    samba-tool dns add 127.0.0.1 au.team admin-dt A 192.168.33.82
-    samba-tool dns add 127.0.0.1 au.team srv1-dt A 192.168.33.66
-    samba-tool dns add 127.0.0.1 au.team srv2-dt A 192.168.33.67
-    samba-tool dns add 127.0.0.1 au.team srv3-dt A 192.168.33.68
-    samba-tool dns add 127.0.0.1 au.team cli-dt A 192.168.33.2
+    samba-tool dns add 127.0.0.1 au.team r-dt A 192.168.33.89 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team fw-dt A 192.168.33.90 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team fw-dt A 192.168.33.1 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team fw-dt A 192.168.33.65 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team fw-dt A 192.168.33.81 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team admin-dt A 192.168.33.82 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team srv1-dt A 192.168.33.66 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team srv2-dt A 192.168.33.67 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team srv3-dt A 192.168.33.68 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team cli-dt A 192.168.33.2 -U Administrator --password=${admin_passwd}
 
-    samba-tool dns add 127.0.0.1 au.team r-hq A 192.168.11.1
-    samba-tool dns add 127.0.0.1 au.team r-hq A 192.168.11.65
-    samba-tool dns add 127.0.0.1 au.team r-hq A 192.168.11.81
-    samba-tool dns add 127.0.0.1 au.team sw1-hq A 192.168.11.82
-    samba-tool dns add 127.0.0.1 au.team sw2-hq A 192.168.11.83
-    samba-tool dns add 127.0.0.1 au.team sw3-hq A 192.168.11.84
-    samba-tool dns add 127.0.0.1 au.team admin-hq A 192.168.11.85
-    samba-tool dns add 127.0.0.1 au.team cli-hq A 192.168.11.2
+    samba-tool dns add 127.0.0.1 au.team r-hq A 192.168.11.1 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team r-hq A 192.168.11.65 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team r-hq A 192.168.11.81 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team sw1-hq A 192.168.11.82 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team sw2-hq A 192.168.11.83 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team sw3-hq A 192.168.11.84 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team admin-hq A 192.168.11.85 -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team cli-hq A 192.168.11.2 -U Administrator --password=${admin_passwd}
 
-    samba-tool dns zonecreate 127.0.0.1 11.168.192.in-addr.arpa
-    samba-tool dns zonecreate 127.0.0.1 33.168.192.in-addr.arpa
-    samba-tool dns zonelist 127.0.0.1
+    samba-tool dns zonecreate 127.0.0.1 11.168.192.in-addr.arpa -U Administrator --password=${admin_passwd}
+    samba-tool dns zonecreate 127.0.0.1 33.168.192.in-addr.arpa -U Administrator --password=${admin_passwd}
+    samba-tool dns zonelist 127.0.0.1 -U Administrator --password=${admin_passwd}
 
-    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 89 PTR r-dt.au.team
+    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 89 PTR r-dt.au.team -U Administrator --password=${admin_passwd}
     samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 90 PTR fw-dt.au.team
-    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 1 PTR fw-dt.au.team
-    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 65 PTR fw-dt.au.team
-    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 81 PTR fw-dt.au.team
-    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 82 PTR admin-dt.au.team
-    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 66 PTR srv1-dt.au.team
-    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 67 PTR srv2-dt.au.team
-    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 68 PTR srv3-dt.au.team
-    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 2 PTR cli-dt.au.team
+    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 1 PTR fw-dt.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 65 PTR fw-dt.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 81 PTR fw-dt.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 82 PTR admin-dt.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 66 PTR srv1-dt.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 67 PTR srv2-dt.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 68 PTR srv3-dt.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 33.168.192.in-addr.arpa 2 PTR cli-dt.au.team -U Administrator --password=${admin_passwd}
 
-    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 1 PTR r-hq.au.team
-    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 65 PTR r-hq.au.team
-    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 66 PTR srv1-hq.au.team
-    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 81 PTR r-hq.au.team
-    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 82 PTR sw1-hq.au.team
-    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 83 PTR sw2-hq.au.team
-    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 84 PTR sw3-hq.au.team
-    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 85 PTR admin-hq.au.team
-    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 2 PTR cli-hq.au.team
+    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 1 PTR r-hq.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 65 PTR r-hq.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 66 PTR srv1-hq.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 81 PTR r-hq.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 82 PTR sw1-hq.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 83 PTR sw2-hq.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 84 PTR sw3-hq.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 85 PTR admin-hq.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 11.168.192.in-addr.arpa 2 PTR cli-hq.au.team -U Administrator --password=${admin_passwd}
 
-    samba-tool dns add 127.0.0.1 au.team www CNAME srv1-dt.au.team -U administrator
-    samba-tool dns add 127.0.0.1 au.team zabbix CNAME srv1-dt.au.team -U administrator
+    samba-tool dns add 127.0.0.1 au.team www CNAME srv1-dt.au.team -U Administrator --password=${admin_passwd}
+    samba-tool dns add 127.0.0.1 au.team zabbix CNAME srv1-dt.au.team -U Administrator --password=${admin_passwd}
 }
 
 add_user_srv-hq () {
@@ -137,10 +137,10 @@ add_user_srv-hq () {
 }
 
 move_clients_srv-hq () {
-    samba-tool computer move ADMIN-DT 'OU=ADMIN,DC=au,DC=team'
-    samba-tool computer move ADMIN-HQ 'OU=ADMIN,DC=au,DC=team'
-    samba-tool computer move CLI-DT 'OU=CLI,DC=au,DC=team'
-    samba-tool computer move CLI-HQ 'OU=CLI,DC=au,DC=team'
+    samba-tool computer move ADMIN-DT 'OU=ADMIN,DC=au,DC=team' -U Administrator --password=${admin_passwd}
+    samba-tool computer move ADMIN-HQ 'OU=ADMIN,DC=au,DC=team' -U Administrator --password=${admin_passwd}
+    samba-tool computer move CLI-DT 'OU=CLI,DC=au,DC=team' -U Administrator --password=${admin_passwd}
+    samba-tool computer move CLI-HQ 'OU=CLI,DC=au,DC=team' -U Administrator --password=${admin_passwd}
 }
 
 shared_folder_srv-hq () {
@@ -149,7 +149,8 @@ shared_folder_srv-hq () {
 
     # Нужно задать параметры в /etc/samba/smb.conf:
     cp /etc/samba/smb.conf /etc/samba/smb.conf.backup   
-    printf "[SAMBA]\n%9spath = /opt/data\n%9scomment = \"SAMBA\"\n%9spublic = yes\n%9swritable = yes\n%9sbrowseable = yes\n%9sguest ok = yes" >> /etc/samba/smb.conf
+#    printf "[SAMBA]\n%9spath = /opt/data\n%9scomment = \"SAMBA\"\n%9spublic = yes\n%9swritable = yes\n%9sbrowseable = yes\n%9sguest ok = yes" >> /etc/samba/smb.conf
+    printf "[SAMBA]\n\tpath = /opt/data\n\tcomment = SAMBA\n\tpublic = yes\n\twritable = yes\n\tbrowseable = yes\n\tguest ok = yes\n" >> /etc/samba/smb.conf
 
     systemctl restart samba
 }
@@ -157,8 +158,7 @@ shared_folder_srv-hq () {
 create_backup_srv-hq () { 
     mkdir /var/bac/
 
-    printf "[Unit]\nDescription=Backup /opt/data\n\n[Service]\nType=oneshot\nExecStart=/bin/tar/ -czf \"/var/bac/SAMBA.tar.gz\" 
-    /opt/data\n\n[Install\nWantedBy=multi-user.target" > /etc/systemd/system/backup.service
+    printf "[Unit]\nDescription=Backup /opt/data\n\n[Service]\nType=oneshot\nExecStart=/bin/tar/ -czf \"/var/bac/SAMBA.tar.gz\" /opt/data\n\n[Install\nWantedBy=multi-user.target" > /etc/systemd/system/backup.service
 
     systemctl daemon-reload
     systemctl enable --now backup.service
@@ -207,41 +207,37 @@ configuring_admin_and_cli () {
     gpupdate-setup enable
 }
 
-samba_select_handler () {
-    samba_select_device_message
-    local choice
-    read choice
-    
-    case "$choice" in
-
-        "1")
-            configuring_srv-hq
-            adding_all_entries_srv-hq
-            add_user_srv-hq
-            move_clients_srv-hq
-            shared_folder_srv-hq
-            ;;
-        "2") 
-            onfiguring_srv-dt
-            ;;
-        "3") 
-            apt-get update && apt-get install -y gpupdate
-            gpupdate-setup enable
-            apt-get update && apt-get install -y admc
-            kinit administrator@AU.TEAM
-            #Нужно адаптировать изменения в интрефейсе под консоль
-            apt-get install -y gpui
-            #Нужно адаптировать изменения в интрефейсе под консоль
-            ;;
-        "4" | "5" | "6")
-            
-
-            ;;
-
-    esac
+function message_select_device() {
+    local var=""
+    while [ -z "${device}" ]; do
+        printf "Выберите устройство:\n 1.SRV1-HQ\ 2.SRV1-DT\n 3.ADMIN-HQ 0.Exit\n"
+            read -r var
+            if [[ ${var} == "1" ]]; then
+                apt-get install task-samba-dc -y
+                configuring_srv-hq
+                adding_all_entries_srv-hq
+                add_user_srv-hq
+                move_clients_srv-hq
+                shared_folder_srv-hq
+            elif [[ ${var} == "2" ]]; then
+                apt-get install task-samba-dc -y
+                onfiguring_srv-dt
+            elif [[ ${var} == "3" ]]; then
+                apt-get install task-samba-dc -y
+                apt-get update && apt-get install -y gpupdate
+                gpupdate-setup enable
+                apt-get update && apt-get install -y admc
+                kinit administrator@AU.TEAM
+                #Нужно адаптировать изменения в интрефейсе под консоль
+                apt-get install -y gpui
+                #Нужно адаптировать изменения в интрефейсе под консоль
+            elif [[ ${var} == "0" ]]; then exit
+            fi
+    done
 }
 
-while true
-do
-    samba_select_device_message
-done
+function main() {
+    message_select_device
+}
+
+main
