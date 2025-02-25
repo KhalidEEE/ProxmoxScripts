9d1a8e60587405bd8a079d213b67964389655a65
KhalidEEELLL <82757724+KhalidEEE@users.noreply.github.com>
2025-02-25 22:45:09 +0300

    upd

--- src/add_user.sh
+++ src/add_user.sh
@@ -14,7 +14,7 @@ DOMAIN=".au.team"
 device=""
 old_hostname="${HOSTNAME}"
 
-device_arr=("sw1-hq" "sw2-hq" "sw3-hq" "admin-hq" "admin-dt" "cli-dt" "cli-hq" "srv1-dt" "srv2-dt" "srv3-dt" "srv1-hq")
+device_arr=("sw1-hq" "sw2-hq" "sw3-hq" "admin-hq" "admin-dt" "cli-dt" "cli-hq" "srv1-dt" "srv2-dt" "srv3-dt" "srv1-hq" "cli")
 
 function set_hostname {
     hostnamectl set-hostname "${device}"${DOMAIN};
@@ -44,7 +44,7 @@ function rollback() {
 function message_select_device() {
     local var=""
     while [ -z "${device}" ]; do
-        printf "Выберите устройство:\n 1.SW1-HQ\n 2.SW2-HQ\n 3.SW3-HQ\n 4.ADMIN-HQ\n 5.ADMIN-DT\n 6.CLI-DT\n 7.CLI-HQ\n 8.SRV1-DT\n 9.SRV2-DT\n 10.SRV3-DT\n 11.SRV1-HQ\n 0.Exit\n"
+        printf "Выберите устройство:\n 1.SW1-HQ\n 2.SW2-HQ\n 3.SW3-HQ\n 4.ADMIN-HQ\n 5.ADMIN-DT\n 6.CLI-DT\n 7.CLI-HQ\n 8.SRV1-DT\n 9.SRV2-DT\n 10.SRV3-DT\n 11.SRV1-HQ\n 12.CLI\n 0.Exit\n"
             read -r var
             if [[ ${var} == "1" ]]; then device=${device_arr[0]}
             elif [[ ${var} == "2" ]]; then device=${device_arr[1]}
@@ -57,6 +57,7 @@ function message_select_device() {
             elif [[ ${var} == "9" ]]; then device=${device_arr[8]}
             elif [[ ${var} == "10" ]]; then device=${device_arr[9]}
             elif [[ ${var} == "11" ]]; then device=${device_arr[10]}
+            elif [[ ${var} == "12" ]]; then device=${device_arr[11]}
             elif [[ ${var} == "0" ]]; then exit
             fi
     done
